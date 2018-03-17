8fea825fde535144bf69cb1b85a1db2619abdcc4
Martijn Smit <smitmartijn@users.noreply.github.com>
2018-03-17 22:08:49 +0100

    - Wrong name. :-(

--- otnsx-activate-middleware.sh
+++ otnsx-activate-middleware.sh
@@ -3,10 +3,10 @@
 # This script should be initiated by the job engine of OTRS.
 #
 # Change this:
-OTRS_URL="http://192.168.178.19:5000"
+OTNSX_URL="http://192.168.178.19:5000"
 
 # Don't modify from here.
 ticketnr=$1
 ticketid=$2
 
-curl -H "Content-Type: application/json" -X POST -d '{"TicketNR":'$ticketnr',"TicketID":'$ticketid'}' $OTRS_URL
+curl -H "Content-Type: application/json" -X POST -d '{"TicketNR":'$ticketnr',"TicketID":'$ticketid'}' $OTNSX_URL
